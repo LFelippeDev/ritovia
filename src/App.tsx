bd19e228b3e492d74c5b607de6e34927a3b41172
Felippe Fernandes <lfelippeeng@gmail.com>
2025-02-24 22:51:14 -0300

    some changes

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Step from './components/Step';
+
+export default function App() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <Step />
+    </main>
+  );
+}
