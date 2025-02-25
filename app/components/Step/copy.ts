80e0fc7709cf768f4c13ae0da92ffa5cc21b253c
Felippe Fernandes <lfelippeeng@gmail.com>
2025-02-24 22:28:10 -0300

    finish changes

---
+++ app/components/Step/copy.ts
@@ -0,0 +1,37 @@
+export const steps = [
+  {
+    title1: 'O Primeiro Ingrediente:',
+    title2: 'O Encontro Perfeito',
+    description:
+      'Todo prato inesquecível começa com os ingredientes certos. Foi assim quando o Ritovia surgiu, um restaurante criado para transformar simples momentos em experiências marcantes. Desde o primeiro contato com nosso menu, cada detalhe foi pensado para surpreender e encantar, assim como aquele primeiro olhar que diz: aqui tem algo especial.',
+  },
+  {
+    title1: 'O Preparo Cuidadoso:',
+    title2: 'Conhecendo Cada Sabor',
+    description:
+      'No Ritovia, cada prato é feito com carinho, explorando temperos e texturas para que cada garfada seja única. Assim como na vida, os melhores relacionamentos não acontecem por acaso — eles são construídos com paciência, descobertas e um toque de surpresa. Aos poucos, os sabores se misturam, criando uma combinação perfeita, assim como nós dois nos aproximamos, encaixando nossas diferenças como uma massa fresca combina com um molho encorpado.',
+  },
+  {
+    title1: 'O Prato Principal:',
+    title2: 'O Momento de Decidir',
+    description:
+      'Toda refeição tem aquele momento em que sentimos que encontramos nosso prato favorito. É aquele instante em que tudo faz sentido, onde cada tempero complementa o outro. E, entre risadas e conversas, entre jantares improvisados e momentos inesquecíveis, percebi que você é o sabor que faltava no meu mundo.',
+  },
+  {
+    title1: 'A Sobremesa:',
+    title2: 'O Doce que Completa Tudo',
+    description:
+      'Depois de um prato bem servido, chega a hora da sobremesa – o toque final que deixa qualquer experiência inesquecível. Mas o mais curioso sobre doces é que, quando são bons de verdade, a gente não quer que acabem. E é assim que me sinto ao seu lado: cada momento com você é doce, envolvente, e eu não quero que termine.',
+  },
+  {
+    title1: 'O Pedido Especial:',
+    title2: 'A Escolha do Coração',
+    description:
+      'O Ritovia não é só um restaurante, é um convite para viver uma experiência única. E hoje, depois de cada prato compartilhado, cada sabor descoberto e cada detalhe apreciado, tenho uma pergunta especial para você:',
+  },
+  {
+    title2: '💍 Quer ser o ingrediente principal da minha história? 💕',
+    description:
+      'Eu te convido a embarcar nessa jornada comigo. Vamos juntos preparar essa receita que, tenho certeza, será a mais deliciosa da minha vida.',
+  },
+];
---
+++ app/styles/theme.ts
@@ -0,0 +1,11 @@
+export const light = {
+  colors: {
+    basilGreen: '#228B22',
+    black: '#000000',
+    burntRed: '#B22222',
+    softCream: '#FAF3E0',
+    snowWhite: '#F0F0F0',
+    white: '#FFFFFF',
+    woodenGold: '#C49A6C',
+  },
+};
